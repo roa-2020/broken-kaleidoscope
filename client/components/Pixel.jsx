de0f1dd6e32820e6c4481c8fa9843d0127a913eb
jgray1 <john.edwin.gray@gmail.com>
2020-08-25 11:52:26 +1200

    complete base exercise

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -2,18 +2,62 @@ import React from 'react'
 
 const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-class Pixel extends React.Component {
+
+  class Pixel extends React.Component {
       
   state = {style: {
-      height: '50px',
-      width: '50px',
+      height: '20px',
+      width: '20px',
       backgroundColor: randomHexColor() 
     }  
    }
+
+   clickHandler = evt => {
+    this.setState({style: {
+      ...this.state.style,
+      backgroundColor: randomHexColor()}}
+    )
+  }
+
+  mouseHandler = evt => {
+    this.setState({style: {
+      ...this.state.style,
+      backgroundColor: 'green'}}
+    )
+  }
+  
+  contextHandler = evt => {
+    evt.preventDefault()
+    console.log('hi')
+    this.setState({style: {
+      ...this.state.style,
+      backgroundColor: 'black'}}
+    )
+  }
+
+  doubleClickHandler = evt => {
+    this.setState({style: {
+      ...this.state.style,
+      backgroundColor: 'white'}}
+    )
+  }
+  
+  dragEnterHandler = evt => {
+    this.setState({style: {
+      ...this.state.style,
+      backgroundColor: 'yellow'}}
+    )
+  }
   
   render () {
-    return <div style ={this.state.style}></div>
+    return <div onClick= {this.clickHandler} 
+    onMouseEnter = {this.mouseHandler}
+    onContextMenu = {this.contextHandler}
+    onDoubleClick = {this.doubleClickHandler}
+    onDragEnter = {this.dragEnterHandler}
+    style ={this.state.style}></div>
   }
 }
+
 
 export default Pixel

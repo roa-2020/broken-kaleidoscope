bbfcd65ddfa46bd43e739907f38c71619b2e2e32
jgray1 <john.edwin.gray@gmail.com>
2021-01-26 11:14:16 +1300

    convert to functional component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Pixel from './Pixel'
 
-const App = () => {
-  let arr = []
-    for ( let i = 0; i < 10000; i++) {
-      arr.push(<Pixel />)
-    }
-  return (
-      arr
-  )
+function App () {
+  // Original
+  // let arr = []
+  //   for ( let i = 0; i < 10000; i++) {
+  //     arr.push(<Pixel key={i}/>)
+  //   }
+  // return (
+  //     arr
+  // )
+  
+  // improved
+  return Array.from( {length: 1000 }, (item, i) => <Pixel key={i} />)
 }
 
 export default App
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,63 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
-  class Pixel extends React.Component {
-      
-  state = {style: {
-      height: '20px',
-      width: '20px',
-      backgroundColor: randomHexColor() 
-    }  
-   }
+const Pixel = () => {
+  
+  const [style, setStyle] = useState({ height: '20px', 
+                                      width: '20px', 
+                                      backgroundColor: randomHexColor() 
+                                    })
 
-   clickHandler = evt => {
-    this.setState({style: {
-      ...this.state.style,
-      backgroundColor: randomHexColor()}}
-    )
+   const clickHandler = evt => {
+      setStyle( {
+      ...style,
+      backgroundColor: randomHexColor()
+    })
   }
 
-  mouseHandler = evt => {
-    this.setState({style: {
-      ...this.state.style,
-      backgroundColor: 'green'}}
+  const mouseHandler = evt => {
+    setStyle({
+      ...style,
+      backgroundColor: 'green'}
     )
   }
   
-  contextHandler = evt => {
+  const contextHandler = evt => {
     evt.preventDefault()
-    console.log('hi')
-    this.setState({style: {
-      ...this.state.style,
-      backgroundColor: 'black'}}
+    setStyle( {
+      ...style,
+      backgroundColor: 'black'}
     )
   }
 
-  doubleClickHandler = evt => {
-    this.setState({style: {
-      ...this.state.style,
-      backgroundColor: 'white'}}
+  const doubleClickHandler = evt => {
+    setStyle({ 
+      ...style,
+      backgroundColor: 'white'}
     )
   }
   
-  dragEnterHandler = evt => {
-    this.setState({style: {
-      ...this.state.style,
-      backgroundColor: 'yellow'}}
+  const dragEnterHandler = evt => {
+    setStyle( {
+      ...style,
+      backgroundColor: 'yellow'}
     )
   }
   
-  render () {
-    return <div onClick= {this.clickHandler} 
-    onMouseEnter = {this.mouseHandler}
-    onContextMenu = {this.contextHandler}
-    onDoubleClick = {this.doubleClickHandler}
-    onDragEnter = {this.dragEnterHandler}
-    style ={this.state.style}></div>
-  }
+  return <div onClick= {clickHandler} 
+    onMouseEnter = {mouseHandler}
+    onContextMenu = {contextHandler}
+    onDoubleClick = {doubleClickHandler}
+    onDragEnter = {dragEnterHandler}
+    style ={style}></div>
 }
-
 
 export default Pixel

5f87cc7b26634f68b7fff8cbc322bd9273fb9ca5
jgray1 <john.edwin.gray@gmail.com>
2021-01-26 16:58:01 +1300

    add jest-dom

--- client/testing/Pixel.test.js
+++ client/testing/Pixel.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Pixel from './../components/Pixel'
 import { screen, render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
-// const style = { height: '20px', width: '20px', backgroundColor: 'red' } 
 const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
@@ -19,7 +19,7 @@ describe('Mouse events change styles', () => {
     fireEvent.click(pixel)
     expect(pixel.style.backgroundColor).not.toBe(expected)
   })
-  
+
   test(`double clicking a tile changes it's color to be white`, () => {
     render(<Pixel />)
     let pixel = screen.getByTestId('pixel')
